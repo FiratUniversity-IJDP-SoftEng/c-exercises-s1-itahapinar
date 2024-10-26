d6dc40704fa2518ec7a3e7082c8443216f752043
İbrahim Taha Pınar <tahapinar32@gmail.com>
2024-10-26 16:18:33 +0300

    Assignments

--- ex3.c
+++ ex3.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main(){
+    printf("Do you have a cat or dog ?\n If you have, please press 1, \n If you don't have, please press 2 : ");
+    int  a ;
+    scanf("%d",&a);
+    if (a == 1)
+    {
+        printf("Which one do you have? \n Dog => 1 Cat => 2 : ");
+        int b;
+        scanf("%d",&b);
+        if (b == 1){
+         printf("Your animal has hav hav hav voice.");
+        }
+        else if (b==2);{
+            printf("Your animal has miyav miyav voice.");
+        }
+        }
+    else{
+        printf("You don't have any pet.");
+    }
+        
+    
+}
--- ex5.c
+++ ex5.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+void sayHello(char name[50]){
+    printf("Please enter your name : ");
+    scanf("%s",name);
+    printf("Hello %s",name);
+   
+}
+
+int main(){
+    char name[50];
+    sayHello(name);
+    
+}
